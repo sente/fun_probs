70f32f470ec8924cc72133aeb8d844ce9094aa7c
Stuart Powers <stuart.powers@gmail.com>
2013-06-11 17:54:22 -0400

    added a shitty readme for all_combinations.py

--- all_combinations.py
+++ all_combinations.py
@@ -1,10 +1,36 @@
 import sys
 sys.setrecursionlimit(10000000)
 
-# http://i.imgur.com/7vWyVE6.png
-# http://c.sente.cc/gLEr/all_combinations.html
+"""
+begin shitty "readme"...
+
+find the shortest string which contains all substrings of length N:
+
+stu@sente ~ python all_combinations.py
+alpha_len:      2
+code_len:       5
+solution_len:   36
+solution:       000001000110010100111010110111110000
+
+there are 2^5 strings that must exist as substrings somewhere in our answer
+00000
+00001
+00010
+...
+...
+11101
+11110
+11111
+
+They are all contained in this string!
+000001000110010100111010110111110000
 
 
+http://i.imgur.com/7vWyVE6.png
+http://c.sente.cc/gLEr/all_combinations.html
+
+
+"""
 
 def getSubstrings(alphabet, sslen, cur=""):
     if len(cur) == len(alphabet) ** sslen + sslen-1:
